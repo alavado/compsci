c4e3a2bff3d8c1609a87c0598a728ed18c4e8269
Alejandro Lavado <alejandro.lvd@gmail.com>
2019-08-25 18:57:18 -0400

    agrega ola superpuesta

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -3,18 +3,24 @@ import './Landing.css'
 
 const Landing = () => {
   return (
-    <div>
-      <div class="contenedor-flex">
-        <div id="contenedor-logo">
-          <h1>Compsci</h1>
-          <p>Desarrollo de apps relacionadas con el mar</p>
+    <React.Fragment>
+      <div id="top-stuff">
+        <div className="wave1"></div>
+        <div className="wave1 overlay"></div>
+        <div id="bottom-bg" className="overlay"></div>
+        <div class="contenedor-flex">
+          <div id="contenedor-logo">
+            <h1>Compsci</h1>
+            <p>Desarrollo de apps relacionadas con el mar</p>
+          </div>
         </div>
+        <div id="top-bg"></div>
+        <div id="bottom-bg"></div>
       </div>
-      <div id="top-bg"></div>
-      <div className="wave1"></div>
-      <div className="wave1 overlay"></div>
-      <div id="bottom-bg"></div>
-    </div>
+      <div id="bla">
+        asdasd
+      </div>
+    </React.Fragment>
   );
 };
 
