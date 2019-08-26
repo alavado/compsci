e6de683b0549e9dcfd650595a517cdd91d98a403
Alejandro Lavado <alejandro.lvd@gmail.com>
2019-08-25 20:56:52 -0400

    los pescaditos ya no se salen por debajo de la pantalla

--- src/components/Landing/Pescaditos/Pescadito.js
+++ src/components/Landing/Pescaditos/Pescadito.js
@@ -4,16 +4,17 @@ import { faFish } from '@fortawesome/free-solid-svg-icons'
 
 const crearPosicionAleatoria = () => ({
   marginLeft: `${100 * Math.random()}vw`,
-  marginTop: `${30 * Math.random()}vh`,
+  marginTop: `${20 * Math.random()}vh`,
 })
 
-const Pescadito = () => {
+const Pescadito = ({comentario}) => {
 
   const [posicion, setPosicion] = useState(crearPosicionAleatoria())
+  const [direccion, setDireccion] = useState('der')
 
   const moverPescadito = () => {
     let nuevaPosicion = crearPosicionAleatoria()
-    nuevaPosicion.transform = nuevaPosicion.marginLeft < posicion.marginLeft ? 'rotateY(.5turn)' : 'rotateY(0turn)'
+    setDireccion(nuevaPosicion.marginLeft < posicion.marginLeft ? 'izq' : 'der')
     setPosicion(nuevaPosicion)
     setTimeout(moverPescadito, 3000 + Math.random() * 2000)
   }
@@ -24,7 +25,14 @@ const Pescadito = () => {
 
   return (
     <div className="pescadito" style={posicion}>
-      <FontAwesomeIcon icon={faFish} size={'4x'} />
+      <div className="comentario-pescadito">
+        <p>{ comentario }</p>
+      </div>
+      <FontAwesomeIcon
+        icon={faFish}
+        size={'4x'}
+        style={{transform: `rotateY(${direccion === 'der' ? '0turn' : '.5turn'})`}}
+      />
     </div>
   );
 };
--- src/components/Landing/Pescaditos/Pescaditos.js
+++ src/components/Landing/Pescaditos/Pescaditos.js
@@ -5,11 +5,11 @@ import Pescadito from './Pescadito';
 const Pescaditos = () => {
   return (
     <div id="pescaditos">
-    <Pescadito />
-    <Pescadito />
-    <Pescadito />
-    <Pescadito />
-    <Pescadito />
+      <Pescadito comentario="aplicaciones web progresivas" />
+      <Pescadito comentario="inteligencia artificial" />
+      <Pescadito comentario="ciencia de datos" />
+      <Pescadito comentario="internet de las cosas" />
+      <Pescadito comentario="aplicaciones nativas" />
     </div>
   );
 };
